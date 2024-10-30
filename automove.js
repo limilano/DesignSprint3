499de227e1933f80a410916f94875c7ed2b2c29b
limilano <limilano@davidson.edu>
2024-10-30 16:11:27 -0400

    wasd controls back

--- automove.js
+++ automove.js
@@ -1,7 +1,7 @@
 
 var jumpscare = false;
 
-AFRAME.registerComponent('auto-move-forward', {
+AFRAME.registerComponent('jumpscare-detection', {
     schema: {
       speed: { type: 'number', default: 0.015 } // Movement speed
     },
@@ -15,15 +15,15 @@ AFRAME.registerComponent('auto-move-forward', {
       const rotationYRad = rotation.y * (Math.PI / 180);
   
       // Calculate forward movement based on the camera's rotation
-      const dx = -Math.sin(rotationYRad) * this.data.speed;
-      const dz = -Math.cos(rotationYRad) * this.data.speed;
+      // const dx = -Math.sin(rotationYRad) * this.data.speed;
+      // const dz = -Math.cos(rotationYRad) * this.data.speed;
   
-      // Update the camera's position
-      this.el.setAttribute('position', {
-        x: position.x + dx,
-        y: position.y,
-        z: position.z + dz
-      });
+      // // Update the camera's position
+      // this.el.setAttribute('position', {
+      //   x: position.x + dx,
+      //   y: position.y,
+      //   z: position.z + dz
+      // });
 
       const newPosition = this.el.getAttribute('position');
       if(!jumpscare && (newPosition.x > 8.8 && newPosition.x < 11.6) && (newPosition.z > 4.2 && newPosition.z < 7.2)) {
@@ -32,6 +32,7 @@ AFRAME.registerComponent('auto-move-forward', {
       }
     }
   });
+
 
   
   function JumpscareAnim() {
